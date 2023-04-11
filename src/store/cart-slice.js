4dacae7137c671cd8f326ef6473b19a7142d445c
VictorScoolExper <victorscool477@outlook.com>
2023-04-11 10:20:52 -0600

    Finished Section 19

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,16 +1,23 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+
 const cartSlice = createSlice({
   name: "cart",
   initialState: {
     items: [],
     totalQuantity: 0,
+    changed: false,
   },
   reducers: {
+    replaceCart(state, action){
+      state.totalQuantity = action.payload.totalQuantity;
+      state.items = action.payload.items;
+    },
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
       state.totalQuantity++;
+      state.changed = true;
       if (!existingItem) {
         // using "push" can only be done using the redux toolkit
         // if you do not use the toolkit it manipulate existing state
@@ -31,6 +38,7 @@ const cartSlice = createSlice({
         const id = action.payload;
         const existingItem = state.items.find(item => item.id === id);
         state.totalQuantity--;
+        state.changed = true;
         if(existingItem.quantity === 1){
             state.items = state.items.filter(item => item.id !== id);
         } else {
